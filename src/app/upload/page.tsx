a9ca239d1a48ff53c44d768fdf44c9b091d9d6c7
Goldeno10 <ibrahimmuhammad271@gmail.com>
2024-08-30 05:48:53 +0100

    commented cloudinary code

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -1,40 +1,48 @@
 "use client";
-import React, { useState } from "react";
-import { CldUploadWidget, CldImage } from "next-cloudinary";
+// import React, { useState } from "react";
+// import { CldUploadWidget, CldImage } from "next-cloudinary";
 
-interface CloudinaryResult {
-  public_id: string;
-}
+// interface CloudinaryResult {
+//   public_id: string;
+// }
 
-const UploadPage = () => {
-  const [publicId, setPublicId] = useState("");
+// const UploadPage = () => {
+//   const [publicId, setPublicId] = useState("");
 
-  return (
-    <>
-      {publicId && (
-        <CldImage src={publicId} width={270} height={300} alt="Test Image" />
-      )}
-      <br />
-      <CldUploadWidget
-        options={{
-          sources: ["local"],
-        }}
-        uploadPreset="rdoj1zir"
-        onSuccess={(result) => {
-          const info = result.info as CloudinaryResult;
-          // console.log("result XXXXXXXXXXXX  ", result);
-          setPublicId(info.public_id);
-        }}
-        onError={(error) => {
-          console.log(error);
-        }}
-      >
-        {({ open }) => {
-          return <button onClick={() => open()}>Upload an Image</button>;
-        }}
-      </CldUploadWidget>
-    </>
-  );
+//   return (
+//     <>
+//       {publicId && (
+//         <CldImage src={publicId} width={270} height={300} alt="Test Image" />
+//       )}
+//       <br />
+//       <CldUploadWidget
+//         options={{
+//           sources: ["local"],
+//         }}
+//         uploadPreset="rdoj1zir"
+//         onSuccess={(result) => {
+//           const info = result.info as CloudinaryResult;
+//           // console.log("result XXXXXXXXXXXX  ", result);
+//           setPublicId(info.public_id);
+//         }}
+//         onError={(error) => {
+//           console.log(error);
+//         }}
+//       >
+//         {({ open }) => {
+//           return <button onClick={() => open()}>Upload an Image</button>;
+//         }}
+//       </CldUploadWidget>
+//     </>
+//   );
+// };
+
+// export default UploadPage;
+
+import React from "react";
+
+const page = () => {
+  return <div>page</div>;
 };
 
-export default UploadPage;
+export default page;
