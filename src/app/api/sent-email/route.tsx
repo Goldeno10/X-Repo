b202514d071c72c1fbf2b5d43e79c0edcebbfa19
Goldeno10 <ibrahimmuhammad271@gmail.com>
2024-08-30 05:44:56 +0100

    commented out resend code

--- src/app/api/sent-email/route.tsx
+++ src/app/api/sent-email/route.tsx
@@ -1,18 +1,21 @@
-import {Resend} from "resend";
-import WelcomeTemplate from "../../../../emails/WelcomeTemplate";
-import { NextResponse } from "next/server";
+// import {Resend} from "resend";
+// import WelcomeTemplate from "../../../../emails/WelcomeTemplate";
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "../../../../prisma/client";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST(){
-    resend.emails.send({
-        from:"...",
-        to:"...",
-        subject:"...",
-        react:<WelcomeTemplate name="John Doe" />
-    })
+// export async function POST(){
+//     resend.emails.send({
+//         from:"...",
+//         to:"...",
+//         subject:"...",
+//         react:<WelcomeTemplate name="John Doe" />
+//     })
 
-    return NextResponse.json({message:"Email sent successfully"},{status:200})
+//     return NextResponse.json({message:"Email sent successfully"},{status:200})
+// }
+
+export async function GET(request: NextRequest) {
+  return NextResponse.json({ seccess: "true" }, { status: 200 });
 }
-
-
